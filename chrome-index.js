7572cce85f49852690d225a747ca14094065455b
bofur24 <dransiktolkien@yahoo.com>
2022-06-15 14:46:13 -0500

    Get data from localStorage

--- chrome-index.js
+++ chrome-index.js
@@ -4,10 +4,15 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+console.log(leadsFromLocalStorage);
+
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     inputEl.value = "" ;
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)); //SETTING INPUT TO LOCALSTORAGE AS A STRING ON BUTTON CLICK
 
 
     renderLeads();
