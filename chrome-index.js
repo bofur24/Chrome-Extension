75aae0c7c978239e8baf8447329f26c61e5e742c
bofur24 <dransiktolkien@yahoo.com>
2022-06-16 14:11:53 -0500

    Refactor my code

--- chrome-index.js
+++ chrome-index.js
@@ -8,15 +8,28 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if(leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
-    renderLeads();
+    render(myLeads);
 }
 
+function render(leads) {
+    let listItems = "";
+    for ( i = 0; i < leads.length; i++) {
+
+        listItems += `
+        <li>
+            <a target="_blank" href="${leads[i]}" > 
+            ${leads[i]}  
+            <a>
+        </li>`;
+    }
+    ulEl.innerHTML = listItems;
+}
 
 
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear();
     myLeads = [];
-    renderLeads();
+    render(myLeads);
 } )
 
 
@@ -25,38 +38,13 @@ inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     inputEl.value = "" ;
     localStorage.setItem("myLeads", JSON.stringify(myLeads)); //SETTING INPUT TO LOCALSTORAGE AS A STRING ON BUTTON CLICK
-
-
-    renderLeads();
+    render(myLeads);
     
 })
 
 
 
-function renderLeads() {
-    let listItems = "";
-    for ( i = 0; i < myLeads.length; i++) {
 
-        //listItems += "<li><a href='" + myLeads[i] +"' target='_blank'>" + myLeads[i] + "<a></li>";
-        //Template strings
-        listItems += `
-        <li>
-            <a target="_blank" href="${myLeads[i]}" > 
-            ${myLeads[i]}  
-            <a>
-        </li>`;
-        
-        
-        
-        // create element
-        //const li = document.createElement("li")
-        // set text content
-        //li.textContent = myLeads[i]
-        // append to ul
-        //ulEl.append(li)
-    }
-    ulEl.innerHTML = listItems;
-}
 
 
 
